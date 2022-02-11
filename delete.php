ea0f35c77f8758de3742ad57b5e54ad464433ed6
Antonio Sarosi <sarosiantonio@gmail.com>
2022-02-11 15:07:34 +0100

    Send 403 if contact does not belong to user

--- delete.php
+++ delete.php
@@ -11,12 +11,20 @@ if (!isset($_SESSION["user"])) {
 
 $id = $_GET["id"];
 
-$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id LIMIT 1");
 $statement->execute([":id" => $id]);
 
 if ($statement->rowCount() == 0) {
   http_response_code(404);
   echo("HTTP 404 NOT FOUND");
+  return;
+}
+
+$contact = $statement->fetch(PDO::FETCH_ASSOC);
+
+if ($contact["user_id"] !== $_SESSION["user"]["id"]) {
+  http_response_code(403);
+  echo("HTTP 403 UNAUTHORIZED");
   return;
 }
 
--- edit.php
+++ edit.php
@@ -22,6 +22,12 @@
 
   $contact = $statement->fetch(PDO::FETCH_ASSOC);
 
+  if ($contact["user_id"] !== $_SESSION["user"]["id"]) {
+    http_response_code(403);
+    echo("HTTP 403 UNAUTHORIZED");
+    return;
+  }
+
   $error = null;
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
