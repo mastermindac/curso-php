e2af4c10e7b71e12c2676cd16a73f5920df7b7a8
Marcos Aparicio <marcos.aparicio593@gmail.com>
2022-06-14 15:44:37 -0500

    fix url bugs with image

--- partials/header.php
+++ partials/header.php
@@ -23,7 +23,7 @@
   <!-- Static Content -->
   <link rel="stylesheet" href="./static/css/index.css" />
   <?php $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) ?>
-  <?php if ($uri == "/contacts-app/" || $uri == "/contacts-app/index.php"): ?>
+  <?php if ($uri == "/contacts_app_dark/" || $uri == "/contacts_app_dark/index.php"): ?>
     <script defer src="./static/js/welcome.js"></script>
   <?php endif ?>
 
